b31faab00444270a57f9b8718a1985f82a956513
Eliza Ahmed <elizarafee@gmail.com>
2021-06-15 13:10:49 +0100

    added css file to style the robots images

--- src/components/robotsCardComponent.js
+++ src/components/robotsCardComponent.js
@@ -1,11 +1,12 @@
 import React from "react";
+import '../style/style.css';
 import "tachyons";
 
 const Robotscardcomponent = ({robot}) => {
   return (
     <>
       <div className="bg-light-blue dib ma3 b--solid bw2 br4 grow shadow-5 tc">
-        <img alt="robot" src={`https://robohash.org/${robot.id}?200*200`} />
+        <img className="moving-left" alt="robot" src={`https://robohash.org/${robot.id}?200*200`} />
         <div>
           <h2>{robot.name}</h2>
           <p>{robot.email}</p>
